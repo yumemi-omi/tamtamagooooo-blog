cf1e6bf38e4c26a1f189eecad59c155949b00051
yumemi-omi <hiro.041310402.kt@gmail.com>
2021-07-03 01:33:02 +0900

    fix:カラー変数を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      base: 'var(--color-base)',
+      main: 'var(--color-main)',
+      sub: 'var(--color-sub)',
+      'main-accent': 'var(--color-main-accent)',
+      'sub-accent': 'var(--color-sub-accent)',
+      peach: 'var(--color-peach)',
+      lavendar: 'var(--color-lavendar)',
+      geranium: 'var(--color-geranium)',
+      'forest-green': 'var(--color-forest-green)',
+    },
   },
   variants: {
     extend: {},
