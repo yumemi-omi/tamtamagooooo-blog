166d2fda3ce91574bf185b6bf88266fa92c70e72
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-01-22 22:57:00 +0900

    fix: tailwindの設定をちょこちょこと

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,21 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
-      base: 'var(--color-base)',
-      main: 'var(--color-main)',
-      sub: 'var(--color-sub)',
-      'main-accent': 'var(--color-main-accent)',
-      'sub-accent': 'var(--color-sub-accent)',
-      peach: 'var(--color-peach)',
-      lavendar: 'var(--color-lavendar)',
-      geranium: 'var(--color-geranium)',
-      'forest-green': 'var(--color-forest-green)',
+      colors: {
+        base: 'var(--color-base)',
+        main: 'var(--color-main)',
+        sub: 'var(--color-sub)',
+        'main-accent': 'var(--color-main-accent)',
+        'sub-accent': 'var(--color-sub-accent)',
+        peach: 'var(--color-peach)',
+        magnolia: 'var(--color-magnolia)',
+        geranium: 'var(--color-geranium)',
+        'forest-green': 'var(--color-forest-green)',
+      },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/line-clamp')],
 }
