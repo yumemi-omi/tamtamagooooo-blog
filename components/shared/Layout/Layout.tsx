a27aa3f3d378ae52a80045adc258c66675c0a02e
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-01-28 01:37:20 +0900

    一旦消す

--- components/shared/Layout/Layout.tsx
+++
@@ -1,9 +0,0 @@
-import { VFC, ReactNode } from 'react'
-
-type LayoutProps = {
-  children: ReactNode
-}
-
-export const Layout: VFC<LayoutProps> = ({ children }) => {
-  return <div className="p-10">{children}</div>
-}
