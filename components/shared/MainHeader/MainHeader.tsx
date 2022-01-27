232ceafc34f09a5402eaf9548df8bcef88affe07
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-01-28 01:06:02 +0900

    fix: トップページへのリンクを設置

--- components/shared/MainHeader/MainHeader.tsx
+++ components/shared/MainHeader/MainHeader.tsx
@@ -1,6 +1,12 @@
 import { VFC } from 'react'
+import Link from 'next/link'
 
-export const MainHeader: VFC = (props) => {
-  console.log({ props })
-  return <header>ヘッダー ヘッダー</header>
+export const MainHeader: VFC = () => {
+  return (
+    <header>
+      <Link href={`/`}>
+        <a>たまごのブログ</a>
+      </Link>
+    </header>
+  )
 }
