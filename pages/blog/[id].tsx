2d4bda053fbcb8946b5311dc69adfd1285655ee0
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-01-28 12:12:37 +0900

    fix: コンテンツがないときの処理を追加

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -44,30 +44,38 @@ export const getStaticProps = async (
 }> => {
   const id = context.params.id as string
   const data = await getBlogDetail(id)
+  if (data.body) {
+    const $ = cheerio.load(data.body)
 
-  const $ = cheerio.load(data.body)
+    $('pre code').each((_, elm) => {
+      const result = hljs.highlightAuto($(elm).text())
+      $(elm).html(result.value)
+      $(elm).addClass('hljs')
+    })
+    $('iframe').each((_, elm) => {
+      const wrapDiv = $(
+        '<div class="hogehogehogehoge" style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;"></div>',
+      )
+      $(elm).wrap(wrapDiv)
+      $(elm)
+        .attr('width', null)
+        .attr('height', null)
+        .attr('style', 'border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;')
+    })
 
-  $('pre code').each((_, elm) => {
-    const result = hljs.highlightAuto($(elm).text())
-    $(elm).html(result.value)
-    $(elm).addClass('hljs')
-  })
-  $('iframe').each((_, elm) => {
-    const wrapDiv = $(
-      '<div class="hogehogehogehoge" style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;"></div>',
-    )
-    $(elm).wrap(wrapDiv)
-    $(elm)
-      .attr('width', null)
-      .attr('height', null)
-      .attr('style', 'border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;')
-  })
-
-  return {
-    props: {
-      blog: data,
-      highlightedBody: $.html(),
-    },
+    return {
+      props: {
+        blog: data,
+        highlightedBody: $.html(),
+      },
+    }
+  } else {
+    return {
+      props: {
+        blog: data,
+        highlightedBody: '',
+      },
+    }
   }
 }
 
