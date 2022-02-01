ded8641c92872b45bf45f1d335e04656c5e399b2
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-02-02 00:50:16 +0900

    サブタイトル

--- components/shared/MainHeader/MainHeader.tsx
+++ components/shared/MainHeader/MainHeader.tsx
@@ -5,7 +5,10 @@ export const MainHeader: VFC = () => {
   return (
     <header className="sticky top-0 z-10 flex items-center justify-center px-10 py-6 shadow-lg bg-magnolia">
       <Link href={`/`}>
-        <a className="text-xl font-bold text-main">とりたま日記</a>
+        <a className="font-bold text-center text-main">
+          <span className="block text-xl">とりたま日記</span>
+          <span className="block mt-1 text-xs">とりあえず書く、たまごであった</span>
+        </a>
       </Link>
     </header>
   )
