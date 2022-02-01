17c3a5ea8f00c6a890aa26366a600db7cc998af8
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-02-01 09:59:18 +0900

    更新

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,15 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 class MyDocument extends Document {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   render() {
     return (
-      <Html>
+      <Html lang="ja">
         <Head>
-          <Script
-            id="goggle-adsense"
-            strategy="lazyOnload"
+          <script
+            async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9104412012929052"
+            crossOrigin="anonymous"
           />
         </Head>
         <body>
