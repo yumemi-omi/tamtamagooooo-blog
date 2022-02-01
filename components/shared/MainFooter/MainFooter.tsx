3b4010d569e39b58b461bf47e2c57c5868bfe370
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-02-01 22:58:07 +0900

    なおしいい

--- components/shared/MainFooter/MainFooter.tsx
+++ components/shared/MainFooter/MainFooter.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link'
 
 export const MainFooter: VFC = () => {
   return (
-    <header className="sticky bottom-0 z-10 flex items-center justify-center px-10 py-6">
+    <footer className="flex items-center justify-center px-10 py-6">
       <Link href={`/policy`}>
         <a className="text-xl font-bold text-main">プライバシーポリシー</a>
       </Link>
-    </header>
+    </footer>
   )
 }
