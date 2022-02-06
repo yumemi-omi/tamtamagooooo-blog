46a9d28b2bd4da8141e13c170eba389c132506e4
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-02-06 19:22:33 +0900

    mainコンポーネントの位置系のstyle修正

--- src/components/shared/Main/Main.tsx
+++ src/components/shared/Main/Main.tsx
@@ -6,5 +6,5 @@ type MainProps = {
 }
 
 export const Main: VFC<MainProps> = ({ children, classNames }) => {
-  return <main className={`p-10 mt-24 flex-grow ${classNames}`}>{children}</main>
+  return <main className={`mt-24 p-12 flex-grow ${classNames}`}>{children}</main>
 }
