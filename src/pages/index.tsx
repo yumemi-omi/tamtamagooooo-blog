7f7992a1dbab59fc14bd85c62af31ae692280c26
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-02-06 19:23:04 +0900

    投稿一覧ページ更新

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,46 @@
-import Link from 'next/link'
 import { GetStaticProps } from 'next'
 import { getBlog } from '@/libs/apiClient'
 import { Blog } from '@/types/microCMS/api/Blog'
-import { BlogCard } from '@/components/screen/blog/BlogCard'
 import { ReactNode, VFC } from 'react'
 import { Seo } from '@/components/shared/Seo'
+import { VerticalLaneLayout } from '@/components/shared/VerticalLaneLayout'
+import { PostSearch } from '@/features/search/components/PostSearch'
+import { Posts } from '@/components/screen/blog/Posts'
 
-type HomeProps = {
-  blogs: Blog[]
+type Props = {
+  posts: Blog[]
   children: ReactNode
 }
 
-const Home: VFC<HomeProps> = ({ blogs }) => {
+const Home: VFC<Props> = ({ posts }) => {
   return (
     <>
       <Seo path="/" title="投稿一覧" description="とりあえず書く、たまごであった" />
-      {blogs.length !== 0 ? (
-        <ul className="grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-8">
-          {/* TODO: リストコンポーネント化 */}
-          {blogs.map((blog) => (
-            <li key={blog.id}>
-              <Link href={`/blog/${blog.id}`}>
-                <a>
-                  <BlogCard blog={blog} />
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <div>
-          {/* TODO: メッセージコンポーネント化 */}
-          投稿は、まだありません。楽しみにしててね！
-        </div>
-      )}
+      <VerticalLaneLayout>
+        <VerticalLaneLayout.Body>
+          {posts.length !== 0 ? (
+            <Posts posts={posts} />
+          ) : (
+            <div>
+              {/* TODO: メッセージコンポーネント化 */}
+              投稿は、まだありません。楽しみにしててね！
+            </div>
+          )}
+        </VerticalLaneLayout.Body>
+        <VerticalLaneLayout.RightSide>
+          <PostSearch />
+        </VerticalLaneLayout.RightSide>
+      </VerticalLaneLayout>
     </>
   )
 }
 
-// データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps: GetStaticProps = async () => {
   const data = await getBlog()
 
   return {
     props: {
-      blogs: data.contents,
+      posts: data.contents,
     },
   }
 }
