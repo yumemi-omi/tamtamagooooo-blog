cb1cef9533d1d7f2c76d54f8c73f83dccb2e3929
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-02-08 23:35:59 +0900

    api route関連の処理を削除

--- aspida.config.js
+++ aspida.config.js
@@ -1,4 +1,3 @@
 module.exports = [
   { input: 'src/api/microcms', baseURL: `https://${process.env.SERVICE_ID}.microcms.io/api/v1` },
-  { input: 'src/api/next', baseURL: `${process.env.APP_ROOT_URL}/api` },
 ]
--- src/libs/apiClient.ts
+++ src/libs/apiClient.ts
@@ -1,16 +1,9 @@
 import axios from 'axios'
 import aspida from '@aspida/axios'
 import microcmsApi from '@/api/microcms/$api'
-import nextApi from '@/api/next/$api'
 
 const microcmsAxiosConfig = {
   timeout: 3000,
   baseURL: `https://${process.env.SERVICE_ID}.microcms.io/api/v1`,
 }
 export const microcmsApiClient = microcmsApi(aspida(axios, microcmsAxiosConfig))
-
-const nextAxiosConfig = {
-  timeout: 3000,
-  baseURL: `${process.env.APP_ROOT_URL}/api`,
-}
-export const nextApiClient = nextApi(aspida(axios, nextAxiosConfig))
