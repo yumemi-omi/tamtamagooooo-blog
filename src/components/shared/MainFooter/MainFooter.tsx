045f0f81b602cdf8f042982abcddc3bfbf2efad4
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-02-10 05:23:23 +0900

    copyright

--- src/components/shared/MainFooter/MainFooter.tsx
+++ src/components/shared/MainFooter/MainFooter.tsx
@@ -3,10 +3,11 @@ import Link from 'next/link'
 
 export const MainFooter: VFC = () => {
   return (
-    <footer className="flex items-center justify-center px-10 py-6">
+    <footer className="flex flex-col items-center justify-center gap-2 px-10 py-6">
       <Link href={`/policy`}>
         <a className="text-xl font-bold text-main">プライバシーポリシー</a>
       </Link>
+      <span>© 2022 とりたま日記. All rights reserved.</span>
     </footer>
   )
 }
