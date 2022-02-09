be87e7ab8481e69ddb31abf14dd046a96f13ccd1
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-02-10 03:51:24 +0900

    カテゴリーバッジ作成

---
+++ src/features/category/components/CategoryBadge/CategoryBadge.tsx
@@ -0,0 +1,14 @@
+import { VFC } from 'react'
+import { Category } from '@/types/microCMS/api/Category'
+
+type Props = {
+  category: Category
+}
+
+export const CategoryBadge: VFC<Props> = ({ category }) => {
+  return (
+    <p className="self-start px-2 py-1 font-bold border border-solid rounded text-sub-accent border-sub-accent min-w-max">
+      {category.name}
+    </p>
+  )
+}
