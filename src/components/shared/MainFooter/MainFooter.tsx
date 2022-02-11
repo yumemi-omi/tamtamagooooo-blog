53c17453dc6fb5f27096f3e52fa5ba60634d690c
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-02-12 00:46:06 +0900

    フッターの一部を浮かせる

--- src/components/shared/MainFooter/MainFooter.tsx
+++ src/components/shared/MainFooter/MainFooter.tsx
@@ -5,9 +5,12 @@ export const MainFooter: VFC = () => {
   return (
     <footer className="flex flex-col items-center justify-center gap-2 px-10 py-6">
       <Link href={`/policy`}>
-        <a className="text-xl font-bold text-main">プライバシーポリシー</a>
+        <a className="z-10 text-xl font-bold text-main">プライバシーポリシー</a>
       </Link>
-      <span>© 2022 とりたま日記. All rights reserved.</span>
+      <div className="z-10 flex flex-wrap items-center justify-center">
+        <span>© 2022 とりたま日記.</span>
+        <span>All rights reserved.</span>
+      </div>
     </footer>
   )
 }
