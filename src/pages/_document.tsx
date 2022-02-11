1b5018e526ebf2fb3617b06a1a24559288943e6e
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-02-11 23:21:50 +0900

    favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,15 @@ class MyDocument extends Document {
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9104412012929052"
             crossOrigin="anonymous"
           ></script>
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="apple-mobile-web-app-title" content="toritama blog" />
+          <meta name="application-name" content="toritama blog" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ad6c6c"></meta>
         </Head>
         <body>
           <Main />
