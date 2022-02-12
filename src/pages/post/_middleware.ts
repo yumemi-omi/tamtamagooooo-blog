98c40e11bfaab6aaaeba80ec6d371887236337d8
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-02-13 06:41:04 +0900

    update

---
+++ src/pages/post/_middleware.ts
@@ -0,0 +1,8 @@
+// pages/_middleware.ts
+
+import type { NextFetchEvent, NextRequest } from 'next/server'
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  console.log('middleware')
+  return new Response('Hello, world!')
+}
