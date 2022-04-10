ff93d7aa1b710dffd3831291a70cf79979e1c1c3
yumemi-omi <38467746+yumemi-omi@users.noreply.github.com>
2022-04-11 00:35:47 +0900

    Delete Tag.ts

--- src/features/tag/types/Tag.ts
+++
@@ -1,7 +0,0 @@
-import { MicroCMSCommonValue } from '@/types/microCMS/common'
-import { TagbadgeColorType } from '@/features/tag/components/TagBadge'
-
-export type Tag = {
-  name: string
-  color: TagbadgeColorType
-} & MicroCMSCommonValue
