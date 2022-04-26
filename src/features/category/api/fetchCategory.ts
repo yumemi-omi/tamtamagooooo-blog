050bca4a1b39d3df548bde49e6c289a8e910e33f
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-04-26 13:42:59 +0900

    fix: カテゴリーの制限を解除

--- src/features/category/api/fetchCategory.ts
+++ src/features/category/api/fetchCategory.ts
@@ -3,5 +3,5 @@ import { headers } from '@/config'
 import { MicroCMSGetQuery } from '@/types/microCMS/query'
 
 export const fetchCategory = (query?: MicroCMSGetQuery) => {
-  return microcmsApiClient.category.$get({ headers, query })
+  return microcmsApiClient.category.$get({ headers, query: { limit: 100, ...query } })
 }
